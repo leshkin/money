d076bbf52094c0b49d872f88510bdd515e14f8a7
leshkin <alaposhin@gmail.com>
2020-04-13 01:55:55 +0300

    fix: SW paths

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 var urlsToCache = [
+  '',
   'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js',
   'https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css',
-  '/index.html',
-  '/index.js'
+  'index.html',
+  'index.js'
 ];
 
 self.addEventListener('install', function(event) {
