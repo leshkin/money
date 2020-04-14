bf1e9b9784a89bb7a57200887cc412f8c4e66f50
leshkin <alaposhin@gmail.com>
2020-04-15 01:28:30 +0300

    chore: update cache

--- sw.js
+++ sw.js
@@ -1,16 +1,11 @@
-var urlsToCache = [
-  '',
-  'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js',
-  'https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css',
-  'index.html',
-  'index.js'
-];
-
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('cache-v1')
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/money/',
+        '/money/index.html',
+        '/money/index.js'
+      ]);
+    })
   );
 });
