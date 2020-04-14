6dbc265384de584fca33020e01a63cb5c104d8fb
leshkin <alaposhin@gmail.com>
2020-04-15 01:48:51 +0300

    fix: cache

--- sw.js
+++ sw.js
@@ -2,10 +2,17 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
-        '/money/',
         '/money/index.html',
         '/money/index.js'
       ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
