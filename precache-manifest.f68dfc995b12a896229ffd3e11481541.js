a531a9ba14de58e9399503c7f76b0e9fb4367926
leshkin <alaposhin@gmail.com>
2021-01-09 19:07:21 +0300

    add PWA support

---
+++ precache-manifest.f68dfc995b12a896229ffd3e11481541.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cab64f0bede4924e4e",
+    "url": "/money/css/app.c0ae05fc.css"
+  },
+  {
+    "revision": "791eb3f18b946e153046",
+    "url": "/money/css/chunk-vendors.253f8355.css"
+  },
+  {
+    "revision": "0b19866af623436ca62552965489640f",
+    "url": "/money/index.html"
+  },
+  {
+    "revision": "63cab64f0bede4924e4e",
+    "url": "/money/js/app.f015a2b0.js"
+  },
+  {
+    "revision": "791eb3f18b946e153046",
+    "url": "/money/js/chunk-vendors.fcac3c6d.js"
+  },
+  {
+    "revision": "58c5f9528babe4fa54dbfb236eb261b9",
+    "url": "/money/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money/robots.txt"
+  }
+]);
